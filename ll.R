281956e8b563d3df5252f70766121578ad88322c
ivamicanovic <125226142+ivamicanovic@users.noreply.github.com>
2023-02-27 10:42:26 +0100

    ll

---
+++ ll.R
@@ -0,0 +1,35 @@
+---
+  title: "Differences Between Serbian and Croatian Pop Music"
+output: 
+  flexdashboard::flex_dashboard
+---
+  
+  ```{r setup, include=FALSE}
+library(flexdashboard)
+```
+
+Column
+-----------------------------------------------------------------------
+  
+  ### Chart A
+  
+  Commentary as it is
+  
+  ```{r} 
+
+```
+
+Column
+-----------------------------------------------------------------------
+  
+  ### Chart B
+  
+  ```{r}
+
+```
+
+### Chart C
+
+```{r}
+
+```
